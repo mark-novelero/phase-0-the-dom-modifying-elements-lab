0b109b1a9c50ff5b0cc6ee951b5546b00f67c6d4
mark novelero <mark.novelero@gmail.com>
2021-03-22 17:07:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+var ele = document.getElementById('main'); 
+ele.remove();
+
+var newHeader = document.createElement('h1');
+document.body.appendChild(newHeader); 
+newHeader.id = 'victory'; 
+newHeader.innerHTML = "<h1>Your-Name is the champion</h1>";
